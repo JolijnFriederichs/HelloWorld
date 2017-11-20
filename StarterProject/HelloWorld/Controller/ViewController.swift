3b8878c03fb7deadb9e839c1cd0fbd2a249586ba
Jolijn Friederichs <jolijn@noodlewerk.com>
2017-11-20 23:28:47 +0100

    Adds the starterproject.

---
+++ StarterProject/HelloWorld/Controller/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Jolijn Friederichs on 08-11-17.
+//  Copyright © 2017 Any. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let personalInfo = PersonalInfo()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ StarterProject/HelloWorld/Model/PersonalInfo.swift
@@ -0,0 +1,15 @@
+//
+//  PersonalInfo.swift
+//  HelloWorld
+//
+//  Created by Jolijn Friederichs on 19-11-17.
+//  Copyright © 2017 Any. All rights reserved.
+//
+
+import Foundation
+
+struct PersonalInfo {
+    
+    var name = "Jolijn"
+    
+}
