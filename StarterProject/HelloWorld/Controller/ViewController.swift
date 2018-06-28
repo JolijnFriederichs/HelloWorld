01db8a0186bcb85dda843006c3fbdb136f18adfd
Jolijn Friederichs <jolijn@noodlewerk.com>
2018-06-28 08:30:58 +0200

    Updates project.

--- StarterProject/HelloWorld/Controller/ViewController.swift
+++ StarterProject/HelloWorld/Controller/ViewController.swift
@@ -10,17 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var shadowView: UIView!
+    @IBOutlet weak var businessCardView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
+
+        shadowView.dropShadow()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
